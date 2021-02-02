22d3d7474ecf44f675837bcdb6dab695b55bd266
JKCamus <924850758@qq.com>
2021-02-02 17:54:56 +0800

    LeetCode 104

---
+++ 算法/104.二叉树的最大深度.js
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=104 lang=javascript
+ *
+ * [104] 二叉树的最大深度
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function (root) {
+  if (!root) return 0;
+  let dep = 0;
+  // 递归寻找左右子节点，如果为null直接就返回 0了
+  dep = 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+  return dep;
+};
+// @lc code=end
