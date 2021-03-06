d98a716bb0fc6da83ca01b81ed9ce14283ef468e
JKCamus <924850758@qq.com>
2021-03-06 20:50:28 +0800

    柯里化优化

--- curry.js
+++ curry.js
@@ -1,24 +1,41 @@
 // 丐版
 const add = function add(x) {
-	return function (y) {
-		return x + y
-	}
-}
-const add1 = add(1)
-add1(2) // 3
-add1(20) //21=20+1
+  return function (y) {
+    return x + y;
+  };
+};
+const add1 = add(1);
+add1(2); // 3
+add1(20); //21=20+1
 // 优化版本
-function curring(){
-  var args=Array.prototype.slice.call(arguments)
-  var fn =function(){
-    var newArgs=args.concat(Array.prototype.slice.call(arguments))
-    return curring.apply(this,newArgs)
-  }
-  fn.toString=function(){
-    return args.reduce(function(a,b){
-      return a*b
-    })
-  }
+function curring() {
+  var args = Array.prototype.slice.call(arguments);
+  var fn = function () {
+    var newArgs = args.concat(Array.prototype.slice.call(arguments));
+    return curring.apply(this, newArgs);
+  };
+  fn.toString = function () {
+    return args.reduce(function (a, b) {
+      return a * b;
+    });
+  };
+  return fn;
+}
+console.log(curring(2)(3)(4)); //24
+/**
+ * @description: es6版本 科里化
+ */
+function test() {
+  let args = Array.prototype.slice.call(arguments);
+  let fn = function () {
+    let newArgs = [...args, Array.prototype.slice.call(arguments)];
+    return test.apply(this, newArgs);
+  };
+  fn.toString = function () {
+    return args.reduce(function (a, b) {
+      return a * b;
+    });
+  };
   return fn
 }
-console.log(curring(2)(3)(4));//24
+console.log(test(2)(3)(4)); //24
