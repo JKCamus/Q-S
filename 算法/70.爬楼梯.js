925d4cb9efa382786b7dd2bcae6898eff8f02b47
JKCamus <924850758@qq.com>
2021-03-11 22:00:18 +0800

    爬楼梯解法二

--- 算法/70.爬楼梯.js
+++ 算法/70.爬楼梯.js
@@ -17,8 +17,21 @@ var climbStairs = function (n) {
   for (let i = 3; i <=n; i++) {
     // 动态规划常用套路
     memo[i] = memo[i - 2] + memo[i - 1];
-    console.log('memo', memo[i])
+    // console.log('memo', memo[i])
   }
   return memo[n];
+// 递归回调 策略更优
+  // const memo = [];
+  // memo[1] = 1;
+  // memo[2] = 2;
+  // function memoize(n) {
+  //   if (memo[n] !== undefined) {
+  //     return memo[n];
+  //   }
+  //   memo[n] = memoize(n - 1) + memoize(n - 2);
+  //   return memo[n]
+  // }
+  // memoize(n);
+  // return memo[n];
 };
 // @lc code=end
