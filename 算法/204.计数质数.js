06f4607b555a5c87b2060dbbf92a960607f2f204
JKCamus <924850758@qq.com>
2021-03-23 13:56:44 +0800

    更改数组填充方式

--- 算法/204.计数质数.js
+++ 算法/204.计数质数.js
@@ -13,9 +13,8 @@
   一个素数的各个倍数，是一个差为此素数本身的等差数列。此为这个筛法和试除法不同  关键之处，后者是以素数来测试每个待测数能否被整除。
  */
 var countPrimes = function (n) {
-
   let count = 0;
-  let arr = new Array(n);
+  let arr = Array(n).fill(0);
   for (let i = 2; i < n; i++) {
     if (!arr[i - 1]) {
       count++;
