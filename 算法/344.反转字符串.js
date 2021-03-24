a100d8d0969999bbf5d64229f2c1ff02dcf371a2
JKCamus <924850758@qq.com>
2021-03-24 10:14:54 +0800

    反转字符串

---
+++ 算法/344.反转字符串.js
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=344 lang=javascript
+ *
+ * [344] 反转字符串
+ */
+
+// @lc code=start
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function (s) {
+  let len=s.length
+  for (let left=0,right=len-1;left<right;left++,right--) {
+    [s[left],s[right]]=[s[right],s[left]]
+  }
+  return s
+
+};
+// @lc code=end
