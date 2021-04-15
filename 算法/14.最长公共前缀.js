158163e81dc9cbbc270355e9c164decc88585f62
JKCamus <924850758@qq.com>
2021-04-15 16:57:16 +0800

    最长公共前缀

---
+++ 算法/14.最长公共前缀.js
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=14 lang=javascript
+ *
+ * [14] 最长公共前缀
+ */
+
+// @lc code=start
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function (strs) {
+  if (strs.length === 0) return "";
+  let res = strs[0];
+  for (let i = 0; i < strs.length; i++) {
+    let j = 0;
+    while (j < res.length && j < strs[i].length && strs[i][j] === res[j]) {
+      j++;
+    }
+    res = res.substr(0, j);
+    if (res === "") {
+      return res;
+    }
+  }
+  return res;
+};
+console.log("res=>", longestCommonPrefix(["flower", "flow", "flight"]));
+// @lc code=end
