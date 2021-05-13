5a9c86dde8bce6ad9fbb270b0d5f339d5eb7d829
JKCamus <924850758@qq.com>
2021-05-13 12:35:28 +0800

    9回文数，不添加额外的空间

--- 算法/9.回文数.js
+++ 算法/9.回文数.js
@@ -11,8 +11,18 @@
  */
 // 双指针也是ok
 var isPalindrome = function (x) {
+  // 212 47m
+  // if (x < 0) return false;
+  // const arr = (x + "").split("");
+  // return arr.join("") === arr.reverse().join("");
+  // 优化
   if (x < 0) return false;
-  const arr = (x + "").split("");
-  return arr.join("") === arr.reverse().join("");
+  let sum = x,
+    res = 0;
+  while (sum !== 0) {
+    res = res * 10 + (sum % 10);
+    sum = Math.floor(sum / 10);
+  }
+  return res === x;
 };
 // @lc code=end
