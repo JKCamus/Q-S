d83fb8736a87d3737b80928046580c8616c7b23c
JKCamus <924850758@qq.com>
2021-05-24 19:44:32 +0800

    415review 字符串相加

--- 算法/415.字符串相加.js
+++ 算法/415.字符串相加.js
@@ -14,17 +14,18 @@ var addStrings = function (num1, num2) {
   let i = num1.length - 1,
     j = num2.length - 1,
     carry = 0,
-    ans = [];
+    res = [];
   while (i >= 0 || j >= 0 || carry !== 0) {
-    let c1 = i >= 0 ? num1.charAt(i) - "0" : 0,
-      c2 = j >= 0 ? num2.charAt(j) - "0" : 0;
+    // 转换为number
+    let c1 = i >= 0 ? num1[i] - "0" : 0;
+    let c2 = j >= 0 ? num2[j] - "0" : 0;
     let sum = c1 + c2 + carry;
-    ans.unshift(sum % 10);
+    res.unshift(sum % 10);
     carry = Math.floor(sum / 10);
     i--;
     j--;
   }
-  return ans.join("");
+  return res.join('');
 };
 // @lc code=end
 // let i = num1.length - 1,
