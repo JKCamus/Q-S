bf5a4301212de47b9ffa9cd59112d84959c0e8fb
JKCamus <924850758@qq.com>
2021-05-26 09:09:56 +0800

    230二叉搜索树中第k小的元素

---
+++ 算法/230.二叉搜索树中第k小的元素.js
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=230 lang=javascript
+ *
+ * [230] 二叉搜索树中第K小的元素
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {number}
+ */
+var kthSmallest = function (root, k) {
+  const stack = [];
+  while (true) {
+    // 直到左叶子节点 没有左节点
+    while (root) {
+      stack.push(root); // 左节点依次入栈
+      root = root.left; // DFS 左节点
+    }
+    root = stack.pop();
+    // 巧妙条件：因为出栈的过程是升序的 故第k次出栈即为BST中第k个最小的元素
+    if (!--k) return root.val;
+    root = root.right; // DFS 左叶子节点的右节点
+  }
+};
+// @lc code=end
