7362c0ac96a6c64dfcbda9a9c27da4dcc4465490
JKCamus <924850758@qq.com>
2021-06-03 14:27:37 +0800

    des: add some des

--- 算法/js常考/千分加逗号.js
+++ 算法/js常考/千分加逗号.js
@@ -4,7 +4,7 @@
  * @Author: camus
  * @Date: 2021-03-03 13:53:14
  * @LastEditors: camus
- * @LastEditTime: 2021-03-03 15:04:45
+ * @LastEditTime: 2021-04-26 19:55:39
  */
 /**
  * @description: 考虑正负，考虑小数
@@ -15,9 +15,13 @@ function numFormat(nums) {
   // 这里需要区别整数负数，正数Math.floor(nums) 负数Math.ceil(nums)
   // return nums>=0 ? Math.floor(num) : Math.ceil(num);
   let result = "";
+  // 正负数flag，用于最后加上
   let flag = nums > 0 ? "" : "-";
+  // 只取正数，转字符串，切成数组
   const numArr = (Math.abs(nums) + "").split(".");
+  // 取出整数部分做处理
   let integerStr = numArr[0];
+  // 大于3位加逗号，并且切割源字符串
   while (integerStr.length > 3) {
     result = "," + integerStr.slice(-3) + result;
     integerStr = integerStr.slice(0, integerStr.length - 3);
@@ -53,3 +57,5 @@ console.log("res=>", numFormat(-1123456789.25585555555));
 // }
 
 // console.log("res2=>", millionFormat(12345678.123456));
+
+
