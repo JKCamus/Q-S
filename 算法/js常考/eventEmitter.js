51c0dcd7a5758f99438415226d654e8b3cbd648b
JKCamus <924850758@qq.com>
2021-06-05 13:37:29 +0800

    review

--- 算法/js常考/eventEmitter.js
+++ 算法/js常考/eventEmitter.js
@@ -4,7 +4,7 @@
  * @Author: camus
  * @Date: 2021-04-28 19:49:06
  * @LastEditors: camus
- * @LastEditTime: 2021-04-28 20:37:06
+ * @LastEditTime: 2021-05-05 21:06:53
  */
 class EventEmitter {
   constructor() {
@@ -24,7 +24,7 @@ class EventEmitter {
       this.events[event] = [];
     }
     this.events[event].push(cb);
-    return this
+    return this;
   }
   off(event, cb) {
     if (!cb) {
@@ -36,17 +36,39 @@ class EventEmitter {
   }
   once(event, cb) {
     const func = (...args) => {
-      this.off(event, func);
       cb.apply(this, args);
+      this.off(event, func);
     };
     this.on(event, func);
     return this;
   }
 }
-const emitter = new EventEmitter()
-emitter.on('log', (param) => {
-  console.log(param)
-})
-emitter.once('log', 'Event Fire')
-emitter.once('log', 'Event Fire')
-emitter.once('log', 'Event Fire')
+const emitter = new EventEmitter();
+emitter.on("log", (param) => {
+  console.log(param);
+});
+emitter.once("log", "Event Fire");
+emitter.once("log", "Event Fire");
+emitter.once("log", "Event Fire");
+
+
+// 第三版
+Array.prototype.fakeReduce = function fakeReduce(fn, base) {
+  if (typeof fn !== "function") {
+    throw new TypeError("arguments[0] is not a function");
+  }
+  let initialArr = this;
+  let arr = initialArr.concat();
+
+  if (base) arr.unshift(base);
+  let index, newValue;
+
+  while (arr.length > 1) {
+    index = initialArr.length - arr.length + 1;
+    newValue = fn.call(null, arr[0], arr[1], index, initialArr);
+
+    arr.splice(0, 2, newValue); // 直接用 splice 实现替换
+  }
+
+  return newValue;
+};
