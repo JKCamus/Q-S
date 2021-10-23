7103e827d92305a39ab642b854c66b64b0224c37
JKcamus <924850758@qq.com>
2021-10-23 10:29:51 +0800

    feat: decorator pattern

---
+++ 算法/project/DecoratorPattern.ts
@@ -0,0 +1,24 @@
+var Plane = function () {};
+Plane.prototype.fire = function () {
+  console.log("发射普通子弹");
+};
+// 增加两个装饰类，分别是导弹和原子弹
+var MissileDecorator = function (plane) {
+  this.plane = plane;
+};
+MissileDecorator.prototype.fire = function (plane) {
+  this.plane.fire();
+  console.log("发射导弹");
+};
+var AtomDecorator = function (plane) {
+  this.plane = plane;
+};
+AtomDecorator.prototype.fire = function () {
+  this.plane.fire();
+  console.log("发射原子弹");
+};
+
+var plane = new Plane();
+var missilePlane = new MissileDecorator(plane);
+
+missilePlane.fire(); // 发射导弹
