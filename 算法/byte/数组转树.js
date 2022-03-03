9c8698a928c625533016fdbf17987f410d88643c
JKcamus <924850758@qq.com>
2022-03-03 18:59:36 +0800

    feat: 数组转树

--- 算法/byte/数组转树.js
+++ 算法/byte/数组转树.js
@@ -8,23 +8,52 @@ let arr = [
 ];
 
 // 递归方案
-const getChildren1 = (data, result, pid) => {
-  for (const item of data) {
-    if (item.pid === pid) {
-      const newItem = { ...item, children: [] };
-      result.push(newItem);
-      getChildren1(data, newItem.children, item.id);
-    }
-  }
-};
-
 const arrayChangeToTree1 = (data, pid = 0) => {
+  const getChildren1 = (data, result, pid) => {
+    for (const item of data) {
+      if (item.pid === pid) {
+        const newItem = { ...item, children: [] };
+        result.push(newItem);
+        getChildren1(data, newItem.children, item.id);
+      }
+    }
+  };
   let result = [];
   getChildren1(data, result, pid);
   return result;
 };
 
-console.log("res=>", JSON.stringify(arrayChangeToTree1(arr)));
+// console.log("res=>", JSON.stringify(arrayChangeToTree1(arr)));
+
+//方案2：有两次循环，该实现的时间复杂度为O(2n)，需要一个Map把数据存储起来，空间复杂度O(n)
+// 主要思路是先把数据转成Map去存储，之后遍历的同时借助对象的引用，直接从Map找对应的数据做存储
+const arrayToTree2 = (data, initPid = 0) => {
+  const result = [];
+
+  const dataMap = {};
+  // 初始化数据map
+  for (const item of data) {
+    dataMap[item.id] = { ...item, children: [] };
+  }
+
+  for (const item of data) {
+    const id = item.id;
+    const pid = item.pid;
+    const treeItem = dataMap[id];
+
+    if (pid === initPid) {
+      result.push(treeItem);
+    } else {
+      if (!dataMap[pid]) {
+        item[pid] = { children: [] };
+      }
+      dataMap[pid].children.push(treeItem);
+    }
+  }
+  return result;
+};
+
+console.log("res2=>", JSON.stringify(arrayToTree2(arr)));
 
 const res = [
   {
