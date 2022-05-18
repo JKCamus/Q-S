cdfd8ce4a751800c934a43526fcc1e343fee98e5
JKCamus <924850758@qq.com>
2022-05-18 22:28:41 +0800

    code: 206 反转链表

---
+++ review/206.反转链表.ts
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=206 lang=typescript
+ *
+ * [206] 反转链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function reverseList(head: ListNode | null): ListNode | null {
+    let pre = null, curr = head, next = head;
+    while (curr !== null) {
+        next = curr.next
+        curr.next = pre
+        pre = curr
+        curr = next
+    }
+    return pre
+
+};
+// @lc code=end
+
