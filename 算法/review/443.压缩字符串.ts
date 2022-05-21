dcd11f8eb59c43373dcd4f18a7f6d96951afd791
JKCamus <924850758@qq.com>
2022-05-21 09:15:07 +0800

    code: 443 压缩字符串

--- 算法/review/443.压缩字符串.ts
+++ 算法/review/443.压缩字符串.ts
@@ -6,25 +6,18 @@
 
 // @lc code=start
 function compress(chars: string[]): number {
-    function compress(chars: string[]): number {
-        const len: number = chars.length
-        for (let z = len - 1; z >= 0; z--) {
-            let count: number = 1
-            while (z > 0 && chars[z] === chars[z - 1]) {
-                count++
-                z--
-            }
-            // 没有重复时不做任何操作
-            if (count !== 1) {
-                // 将统计数量拆为数组
-                let tempArr = count.toString().split('')
-                // 保留当前字符，将重复部分替换为统计数
-                chars.splice(z + 1, count - 1, ...tempArr)
-            }
-        }
-        return chars.length
-    };
-
+  for (let i = chars.length - 1; i >= 0; i--) {
+    let sum: number = 1
+    while(i>0&&chars[i]===chars[i-1]){
+      sum+=1
+      i--
+    }
+    if (sum!==1) {
+      const counterArr=String(sum).split('')
+      chars.splice(i+1,sum-1,...counterArr)
+    }
+  }
+  return chars.length
 };
 // @lc code=end
 
