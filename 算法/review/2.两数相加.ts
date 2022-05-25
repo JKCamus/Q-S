f18235bcd401f447e2eb6d7e800539eb68e20928
JKCamus <924850758@qq.com>
2022-05-25 22:32:01 +0800

    code: 2两数相加

--- 算法/review/2.两数相加.ts
+++ 算法/review/2.两数相加.ts
@@ -21,29 +21,38 @@ function addTwoNumbers(
   l1: ListNode | null,
   l2: ListNode | null
 ): ListNode | null {
-  let dummy = new ListNode();
-  let curr = dummy;
+
   let carry = 0;
+  let dummy = new ListNode()
+  let curr = dummy
+
   while (l1 !== null || l2 !== null) {
-    let sum = 0;
+    let sum = 0
     if (l1 !== null) {
-      sum += l1.val;
-      l1 = l1.next;
+      sum += l1.val
+      // 加了，指针需要移位的
+      l1 = l1.next
     }
 
     if (l2 !== null) {
-      sum += l2.val;
-      l2 = l2.next;
+      sum += l2.val
+      l2 = l2.next
     }
-    sum += carry;
-    curr.next = new ListNode(sum % 10);
-    carry = Math.floor(sum / 10);
-    curr = curr.next;
-  }
-  if (carry > 0) {
-    curr.next = new ListNode(carry);
+
+    sum += carry
+    curr.next = new ListNode(sum % 10)
+    carry = Math.floor(sum / 10)
+    // 注意指针移位
+    curr=curr.next
   }
 
-  return dummy.next;
+  // 进位大于0
+  if (carry > 0) {
+    curr.next = new ListNode(carry)
+  }
+
+  return dummy
+
+
 }
 // @lc code=end
