0c4f514c9208868d5147bab7b59517fbeaefae4a
JKCamus <924850758@qq.com>
2022-05-26 22:13:22 +0800

    code: 24两两交换链表中的节点

---
+++ 算法/review/24.两两交换链表中的节点.ts
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=24 lang=typescript
+ *
+ * [24] 两两交换链表中的节点
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function swapPairs(head: ListNode | null): ListNode | null {
+  let dummy = new ListNode()
+  dummy.next = head
+  let curr = dummy
+  while (curr.next !== null || curr.next.next !== null) {
+    let l1 = curr.next;
+    let l2 = curr.next.next;
+    curr.next = l2
+    l1.next = l2.next
+    l2.next = l1
+    curr = l1
+  }
+  return dummy.next
+
+};
+// @lc code=end
+
