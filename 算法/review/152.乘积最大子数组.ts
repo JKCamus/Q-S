decdc6967afca7032e7bb2f6ddaabde9606ab5f4
JKCamus <924850758@qq.com>
2022-06-03 15:43:43 +0800

    code: 152 乘积最大子数组

---
+++ 算法/review/152.乘积最大子数组.ts
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=152 lang=typescript
+ *
+ * [152] 乘积最大子数组
+ */
+
+// @lc code=start
+function maxProduct(nums: number[]): number {
+  const maxProductMemo = []
+  const minProductMemo = []
+
+  maxProductMemo[0] = nums[0]
+  minProductMemo[0] = nums[0]
+  let max = nums[0]
+  for (let i = 1; i < nums.length; i++) {
+    maxProductMemo[i] = Math.max(maxProductMemo[i - 1] * nums[i], minProductMemo[i - 1] * nums[i], nums[i])
+    minProductMemo[i] = Math.min(maxProductMemo[i - 1] * nums[i], minProductMemo[i - 1] * nums[i], nums[i])
+    max = Math.max(maxProductMemo[i],max)
+  }
+  return max
+
+
+};
+// @lc code=end
+
+console.log(maxProduct([2,3,-2,4]));
