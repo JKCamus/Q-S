8db2fe0bba37b3af843830d0b60dc34bdbb3fb94
JKCamus <924850758@qq.com>
2022-06-04 15:17:17 +0800

    code:283 移动0

---
+++ 算法/review/283.移动零.ts
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=283 lang=typescript
+ *
+ * [283] 移动零
+ */
+
+// @lc code=start
+/**
+ Do not return anything, modify nums in-place instead.
+ */
+function moveZeroes(nums: number[]): void {
+  let j = 0
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      nums[j] = nums[i]
+      j += 1
+    }
+  }
+  for (let i = j; i < nums.length; i++) {
+    nums[i] = 0
+  }
+};
+// @lc code=end
+
