87e42787614ba6cb9c45236ae508b306b715a010
JKCamus <924850758@qq.com>
2022-07-17 23:12:34 +0800

    feat: apply bind

--- base/new-call-apply-bind.js
+++ base/new-call-apply-bind.js
@@ -10,16 +10,38 @@ function create() {
 }
 // 构造函数中有返回值且为对象，那么创建的实例就只能访问到返回对象中的属性
 // 构造函数中没有返回值，那么创建的实例就能访问到这个构造函数中的所有属性了。
-function Person (name) {
-  this.name = name
+function Person(name) {
+  this.name = name;
   // 无返回值
   // return {name:this.name}
 }
 Person.prototype.eat = function () {
-  console.log('Eatting')
-}
+  console.log("Eatting");
+};
 
-const c = create(Person, 'LinDaiDai')
-console.log(c) // Person{ name: 'LinDaiDai' }
+const c = create(Person, "LinDaiDai");
+console.log(c); // Person{ name: 'LinDaiDai' }
 
+Function.prototype.apply = function (con, arr) {
+  if (typeof this !== "function") {
+    throw TypeError("must be function");
+  }
+  const context = con === null || con === undefined ? window : Object(con);
+  const fn = Symbol();
+  context[fn] = this;
+  const result = Array.isArray(arr) ? context[fn](...arr) : context[fn]();
+  delete context[fn];
+  return result;
+};
 
+Function.prototype.bind = function (con) {
+  if (typeof this !== "function") {
+    throw TypeError("error");
+  }
+  const context = con === null || con === undefined ? window : Object(con);
+  const fn = this,
+    args = [...arguments].slice(1);
+  return function Fn() {
+    return fn.apply(this instanceof Fn ? context : args.concat(...arguments));
+  };
+};
